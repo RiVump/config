23c2fb72a03b3bebc2106c5bcfaf4397493e4049
invinciblewest <invinciblewest@gmail.com>
2021-04-02 13:45:18 +0600

    Config class

---
+++ src/Config.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace RiVump\Util;
+
+class Config
+{
+    /**
+     * @var array
+     */
+    private static $configContainer = [];
+
+    /**
+     * @param string $key
+     * @param $value
+     */
+    public static function set(string $key, $value): void
+    {
+        static::$configContainer[$key] = $value;
+    }
+
+    /**
+     * @param string $key
+     * @param null $default
+     * @return mixed|null
+     */
+    public static function get(string $key, $default = null)
+    {
+        return static::$configContainer[$key] ?? $default;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getAll(): array
+    {
+        return static::$configContainer;
+    }
+
+    /**
+     * @param string $namespace
+     * @param bool $withNamespace
+     * @return array
+     */
+    public static function getByNamespace(string $namespace, bool $withNamespace = true): array
+    {
+        $result = [];
+
+        foreach (static::$configContainer as $key => $value) {
+            if (strpos($key, $namespace) === false) {
+                continue;
+            }
+
+            $result[$withNamespace ? $key : str_replace($namespace.'.', '', $key)] = $value;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string|string[] $path
+     */
+    public static function load($path): void
+    {
+        if (is_string($path)) {
+            $path = [$path];
+        }
+
+        $normalizedDocumentRoot = str_replace('\\', '/', dirname(__FILE__, 4));
+
+        foreach ($path as $file) {
+            $config = require $file;
+            if (!$config || !is_array($config)) {
+                continue;
+            }
+
+            $normalizedPath = str_replace('\\', '/', $file);
+            $namespace = str_replace(
+                [$normalizedDocumentRoot.'/ilab/config/', '.php', '/'],
+                ['', '', '.'],
+                $normalizedPath
+            );
+
+            foreach ($config as $key => $value) {
+                static::set($namespace.'.'.$key, $value);
+            }
+        }
+    }
+}
